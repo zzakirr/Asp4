45615bcd9a03f55a63500b78259b2bf714faa4bb
zzakirr <7nez6ws@code.edu.az>
2022-06-26 00:28:11 +0400

    pustok

---
+++ Pustok/Pustok/Migrations/20220625202459_ControlDatabase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pustok.Migrations
+{
+    public partial class ControlDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
